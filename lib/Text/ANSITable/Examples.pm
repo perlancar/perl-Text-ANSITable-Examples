8b5f9a7251a0cdfeb15bc3a17ed7f44f10c2ae7c
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-21 08:36:55 +0700

    Initial commit

---
+++ lib/Text/ANSITable/Examples.pm
@@ -0,0 +1,26 @@
+package Text::ANSITable::Examples;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Examples for Text::ANSITable
+
+=head1 SYNOPSIS
+
+See the included scripts.
+
+
+=head1 DESCRIPTION
+
+This distribution includes the following example scripts that demo
+L<Text::ANSITable>:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Text::ANSITable>
